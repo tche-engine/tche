1e92b9fcd4d4790842fafa03d8fddc75ceada5a2
Guilherme Gazzo <guilhermegazzo@gmail.com>
2016-01-04 15:12:44 -0200

    criada task para gerar o pacote desktop

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,53 +50,73 @@ module.exports = function(grunt) {
     },
     'http-server': {
 
-        'dev': {
+      'dev': {
 
-            // the server root directory
-            root: "./",
+        // the server root directory
+        root: "./",
 
-            // the server port
-            // can also be written as a function, e.g.
-            // port: function() { return 8282; }
-            port: 8080,
+        // the server port
+        // can also be written as a function, e.g.
+        // port: function() { return 8282; }
+        port: 8080,
 
-            // the host ip address
-            // If specified to, for example, "127.0.0.1" the server will
-            // only be available on that ip.
-            // Specify "0.0.0.0" to be available everywhere
-            host: "0.0.0.0",
+        // the host ip address
+        // If specified to, for example, "127.0.0.1" the server will
+        // only be available on that ip.
+        // Specify "0.0.0.0" to be available everywhere
+        host: "0.0.0.0",
 
-            cache: 1,
-            showDir : true,
-            autoIndex: true,
+        cache: 1,
+        showDir: true,
+        autoIndex: true,
 
-            // server default file extension
-            ext: "html",
+        // server default file extension
+        ext: "html",
 
-            // run in parallel with other tasks
-            runInBackground: false,
+        // run in parallel with other tasks
+        runInBackground: false,
 
-            // specify a logger function. By default the requests are
-            // sent to stdout.
-            logFn: function(req, res, error) { },
+        // specify a logger function. By default the requests are
+        // sent to stdout.
+        logFn: function(req, res, error) {},
 
-            // Proxies all requests which can't be resolved locally to the given url
-            // Note this this will disable 'showDir'
-            // proxy: "http://someurl.com",
+        // Proxies all requests which can't be resolved locally to the given url
+        // Note this this will disable 'showDir'
+        // proxy: "http://someurl.com",
 
-            /// Use 'https: true' for default module SSL configuration
-            /// (default state is disabled)
-            // https: {
-            //     cert: "cert.pem",
-            //     key : "key.pem"
-            // },
+        /// Use 'https: true' for default module SSL configuration
+        /// (default state is disabled)
+        // https: {
+        //     cert: "cert.pem",
+        //     key : "key.pem"
+        // },
 
-            // Tell grunt task to open the browser
-            // openBrowser : true
+        // Tell grunt task to open the browser
+        // openBrowser : true
 
-        }
+      }
 
-    }
+    },
+    nwjs: {
+      options: {
+        platforms: ['linux',"win","osx"],
+        buildDir: './generated', // Where the build version of my NW.js app is saved
+        version: '0.12.0'
+      },
+      src: ['./tmp/**'] // Your NW.js app
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          {
+            expand: true,
+            src: ['dist/**',"sample-game/**",'package.json',"engine/libs/**"],
+            dest: 'tmp/'
+          },
+        ],
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -104,9 +124,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-http-server');
-
+  grunt.loadNpmTasks('grunt-nw-builder');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.registerTask('default', ['jshint', 'concat']);
   grunt.registerTask('server', ['http-server']);
+  grunt.registerTask('build', ['copy','nwjs']);
   grunt.registerTask('prod', ['jshint', 'concat', 'uglify']);
 
 };
