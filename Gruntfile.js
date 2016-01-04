5dc12a73668dee68280fe3f62b009ffc5d790e8c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2016-01-04 11:16:35 -0200

    task server

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,55 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['default']
+    },
+    'http-server': {
+
+        'dev': {
+
+            // the server root directory
+            root: "./",
+
+            // the server port
+            // can also be written as a function, e.g.
+            // port: function() { return 8282; }
+            port: 8080,
+
+            // the host ip address
+            // If specified to, for example, "127.0.0.1" the server will
+            // only be available on that ip.
+            // Specify "0.0.0.0" to be available everywhere
+            host: "0.0.0.0",
+
+            cache: 1,
+            showDir : true,
+            autoIndex: true,
+
+            // server default file extension
+            ext: "html",
+
+            // run in parallel with other tasks
+            runInBackground: false,
+
+            // specify a logger function. By default the requests are
+            // sent to stdout.
+            logFn: function(req, res, error) { },
+
+            // Proxies all requests which can't be resolved locally to the given url
+            // Note this this will disable 'showDir'
+            // proxy: "http://someurl.com",
+
+            /// Use 'https: true' for default module SSL configuration
+            /// (default state is disabled)
+            // https: {
+            //     cert: "cert.pem",
+            //     key : "key.pem"
+            // },
+
+            // Tell grunt task to open the browser
+            // openBrowser : true
+
+        }
+
     }
   });
 
@@ -54,9 +103,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-http-server');
 
-  
   grunt.registerTask('default', ['jshint', 'concat']);
+  grunt.registerTask('server', ['http-server']);
   grunt.registerTask('prod', ['jshint', 'concat', 'uglify']);
 
 };
