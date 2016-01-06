d7cf891c69338c760f61fb71ee8713d89427cbe1
Guilherme Gazzo <guilhermegazzo@gmail.com>
2016-01-06 21:42:41 -0200

    som

--- sample-game/mainGame/main.js
+++ sample-game/mainGame/main.js
@@ -1,10 +1,13 @@
 TCHE.InputManager.addKeyAlias('F2', "FPS");
 
-TCHE.on("ready", function(){ 
+TCHE.on("ready", function(){
   TCHE.globals.player.on('blockedBy', function(character){
     var oldImage = TCHE.globals.player.image;
     TCHE.globals.player.image = character.image;
     character.image = oldImage;
+
+    createjs.Sound.play("sound");
+    
   });
 });
 TCHE.on("started", function(){ console.log("started");});
@@ -13,3 +16,5 @@ TCHE.on("started", function(){ console.log("started");});
 TCHE.init({
 });
 
+
+createjs.Sound.registerSound({src:"assets/bookClose.ogg", id:"sound"});
