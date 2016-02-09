176294caf66727a46f7a92ff0a6d5912db3e6720
Pierre H. Lehnen <git@hudell.com>
2016-02-09 00:28:51 -0200

    Improved validations

--- engine/helpers/Validation.js
+++ engine/helpers/Validation.js
@@ -1,13 +1,53 @@
 (function(){
   class Validation {
+    static hasAnySkin() {
+      if (!TCHE.data.game.skins) {
+        return false;
+      }
+
+      for (var key in TCHE.data.game.skins) {
+        return true;
+      }
+
+      return false;
+    }
+
+    static hasAnySprite() {
+      if (!TCHE.data.game.sprites) {
+        return false;
+      }
+
+      for (var key in TCHE.data.game.sprites) {
+        return true;
+      }
+
+      return false;
+    }
+
+    static hasPlayerSprite() {
+      if (!TCHE.data.game.player.sprite) {
+        return false;
+      }
+
+      if (!TCHE.data.game.sprites[TCHE.data.game.player.sprite]) {
+        return false;
+      }
+
+      return true;
+    }
+
     static checkBasicFiles() {
       try{
-        if (!TCHE.data.game.skins || TCHE.data.game.skins.length === 0) {
-          throw new Error("There's no skin configured.");
+        // if (!this.hasAnySkin()) {
+        //   throw new Error("There's no skin configured.");
+        // }
+
+        if (!this.hasAnySprite()) {
+          throw new Error("There's no sprite configured.");
         }
 
-        if (!TCHE.data.game.sprites || TCHE.data.game.sprites.length === 0) {
-          throw new Error("There's no sprite configured.");
+        if (!this.hasPlayerSprite()) {
+          throw new Error("The player's sprite doesn't exist.");
         }
       }
       catch(e) {
